a67b35641855e4454c6768429450358cd590d070
MahithaGS <132842048+MahithaGS@users.noreply.github.com>
2025-02-01 21:10:25 +0530

    Update speech_synthesis.py

--- src/speech_synthesis.py
+++ src/speech_synthesis.py
@@ -1,1 +1,13 @@
+from gtts import gTTS
+import os
+import time
+
+# Function for real-time TTS (text-to-speech)
+def speak(text):
+    speech = gTTS(text=text, lang='en', slow=False)
+    file_name = "temp_output.mp3"
+    speech.save(file_name)
+    os.system(f"start {file_name}")  # For Windows
+    time.sleep(1)
+    os.remove(file_name)
 
